7540a5e7906e5cc4b5d8665679abc326c6d3ab48
Erlend Hamnaberg <erlend@hamnaberg.net>
2026-02-27 21:08:07 +0100

    Try compressing the binaries

--- project.scala
+++ project.scala
@@ -3,8 +3,8 @@
 //> using buildInfo
 //> using computeVersion "git:dynver"
 //> using dep "org.typelevel::cats-effect:3.6.3"
-//> using dep "com.monovore::decline-effect:2.5.0"
-//> using dep "ch.epfl.scala::bloop-rifle:2.0.17"
-//> using dep "io.get-coursier::coursier:2.1.25-M21"
+//> using dep "com.monovore::decline-effect:2.6.0"
+//> using dep "ch.epfl.scala::bloop-rifle:2.0.19"
+//> using dep "io.get-coursier::coursier:2.1.25-M23"
 //> using dep "org.apache.commons:commons-exec:1.6.0"
 //> using dep "io.circe::circe-jawn:0.14.15"
